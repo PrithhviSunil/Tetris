e594a53a6cdec5766f9f43013dcfa9798b7c7346
Prithhvi Sunil <prithhvisunil3007@gmail.com>
2025-07-17 10:04:00 -0400

    First commit, Completed Step 1 and started Step 2

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+from shapes import Shapes_list, Colours_list
+pygame.init()
+
+# Set up display
+WIDTH, HEIGHT = 200, 400
+WHITE = (255, 255,255)
+BLACK = (0,0,0)
+
+#create a board
+Board_width = 20
+Board_height = 40
+board = [[0 for j in range(Board_width)] for i in range(Board_height)]
+
+Screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tetris")
+
+
+
+def draw_grid():
+    block_size = 20
+    for i in range(Board_height):
+        for j in range(Board_width):
+            #get pixel coordinates for each block
+            x = j*block_size
+            y = i*block_size
+            #draw rectangles
+            rectangle = pygame.Rect(x, y, block_size, block_size)
+            if board[i][j]!=0: #get colours
+                colour = Colours_list[board[i][j]-1]
+                pygame.draw.rect(Screen,colour,rectangle)
+            pygame.draw.rect(Screen, WHITE, rectangle, 1)
+
+# Main loop
+running = True
+while running:
+    Screen.fill(BLACK)
+    draw_grid()
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
